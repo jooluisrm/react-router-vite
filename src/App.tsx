32e872daf2420686b4a73b6494cbe362fb7fed24
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-04 10:48:41 -0300

    Rotas em componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { NotFound } from "./pages/NotFound";
-import { AboutItem } from "./pages/AboutItem";
-import { RequireAuth } from "./RequireAuth";
-
+import { MainRoutes } from "./routes/MainRoutes";
 
 const App = () => {
     return (
@@ -15,14 +9,7 @@ const App = () => {
 
             <hr />
             <div className="py-4">
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/sobre" element={<RequireAuth><About /></RequireAuth>} />
-                    <Route path="/sobre/:slug" element={<AboutItem />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-
+                <MainRoutes />
             </div>
             <hr />
 
