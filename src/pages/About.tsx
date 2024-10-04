d8481d85e01422fb84d1c642c366d2c92c19ac5d
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-04 10:25:16 -0300

    Parâmetros de query

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export const About = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const setOrder = (order: 'asc' | 'desc') => {
+        searchParams.set('order', order);
+        setSearchParams(searchParams);
+    }
+
     return (
         <div>
-            Pagina SOBRE
+
+            Filtro: {searchParams.get('filter')} <br />
+            Orderm: {searchParams.get('order')}
+            <hr />  
+            <button onClick={() => setOrder('asc')}>Asc</button>
+            <button onClick={() => setOrder('desc')}>Desc</button> <br />
+
+            Pagina sobre:
             <ul>
                 <li><Link to="/sobre/bonieky">Bonieky</Link></li>
                 <li><Link to="/sobre/pedro">Pedro</Link></li>
